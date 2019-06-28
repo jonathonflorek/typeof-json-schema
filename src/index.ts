8a44c6e0d50a97118be39ba97cd419c335b32515
jonathonflorek <jonathonflorek@gmail.com>
2019-06-27 21:48:12 -0600

    added allof, etc

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,45 @@
-import { JsonArray, JsonObject, JsonPrimitive, JsonValue } from './json';
+import { JsonArray, JsonObject, JsonValue } from './json';
 
 export type ToInterface<T> = InterfaceOf<T, { '#': T; [key: string]: unknown }>;
-type InterfaceOf<T, X> = 
+type InterfaceOf<T, X> =
+    T extends { '$ref': infer R } ? InterfaceOf2<Select<X, R>, X> :
+    T extends { allOf: infer R } ? 
+        R extends Tuple<1> ? InterfaceOf2<R[0], X> :
+        R extends Tuple<2> ? InterfaceOf2<R[0], X> & InterfaceOf2<R[1], X> :
+        R extends Tuple<3> ? InterfaceOf2<R[0], X> & InterfaceOf2<R[1], X> & InterfaceOf2<R[2], X> :
+        R extends Tuple<4> ? InterfaceOf2<R[0], X> & InterfaceOf2<R[1], X> & InterfaceOf2<R[2], X> & InterfaceOf2<R[3], X> :
+        R extends Tuple<5> ? InterfaceOf2<R[0], X> & InterfaceOf2<R[1], X> & InterfaceOf2<R[2], X> & InterfaceOf2<R[3], X> & InterfaceOf2<R[4], X>:
+        JsonValue :
+    T extends { anyOf: readonly (infer R)[] } ? InterfaceOf2<R, X> :
+    T extends { not: infer R } ? Exclude<JsonObject, InterfaceOf2<R, X>> :
+    ValueOf<T, X>;
+type InterfaceOf2<T, X> =
+    T extends { '$ref': infer R } ? InterfaceOf3<Select<X, R>, X> :
+    T extends { allOf: infer R } ? 
+        R extends Tuple<1> ? InterfaceOf3<R[0], X> :
+        R extends Tuple<2> ? InterfaceOf3<R[0], X> & InterfaceOf3<R[1], X> :
+        R extends Tuple<3> ? InterfaceOf3<R[0], X> & InterfaceOf3<R[1], X> & InterfaceOf3<R[2], X> :
+        R extends Tuple<4> ? InterfaceOf3<R[0], X> & InterfaceOf3<R[1], X> & InterfaceOf3<R[2], X> & InterfaceOf3<R[3], X> :
+        R extends Tuple<5> ? InterfaceOf3<R[0], X> & InterfaceOf3<R[1], X> & InterfaceOf3<R[2], X> & InterfaceOf3<R[3], X> & InterfaceOf3<R[4], X>:
+        JsonValue :
+    T extends { anyOf: readonly (infer R)[] } ? InterfaceOf3<R, X> :
+    T extends { not: infer R } ? Exclude<JsonObject, InterfaceOf3<R, X>> :
+    ValueOf<T, X>;
+type InterfaceOf3<T, X> =
+    T extends { '$ref': infer R } ? ValueOf<Select<X, R>, X> :
+    T extends { allOf: infer R } ?
+        R extends Tuple<1> ? ValueOf<R[0], X> :
+        R extends Tuple<2> ? ValueOf<R[0], X> & ValueOf<R[1], X> :
+        R extends Tuple<3> ? ValueOf<R[0], X> & ValueOf<R[1], X> & ValueOf<R[2], X> :
+        R extends Tuple<4> ? ValueOf<R[0], X> & ValueOf<R[1], X> & ValueOf<R[2], X> & ValueOf<R[3], X> :
+        R extends Tuple<5> ? ValueOf<R[0], X> & ValueOf<R[1], X> & ValueOf<R[2], X> & ValueOf<R[3], X> & ValueOf<R[4], X>:
+        JsonValue :
     T extends { anyOf: readonly (infer R)[] } ? ValueOf<R, X> :
+    T extends { not: infer R } ? Exclude<JsonObject, ValueOf<R, X>> :
     ValueOf<T, X>;
 type ValueOf<T, X> =
+    T extends { const: infer C } ? C :
+    T extends { enum: readonly (infer R)[] } ? R :
     T extends { type: 'null' } ? null :
     T extends { type: 'boolean' } ? boolean :
     T extends { type: 'integer' } ? number :
@@ -27,12 +62,38 @@ type ValueOf<T, X> =
         T extends { items: any[] } ? {
             [K in keyof T['items']]: InterfaceOf<T['items'][K], X>;
         } :
-        T extends { items: infer I } ? ArrayOf<T, X> :
+        T extends { items: infer I } ? ArrayOf<I, X> :
         JsonArray :
     JsonValue;
 interface ArrayOf<T, X> extends Array<InterfaceOf<T, X>> {}
 
-const thing = {
+type Select<T, K> =
+    K extends Tuple<0> ? T :
+    K extends Tuple<1> ? T [K[0]] :
+    K extends Tuple<2> ? T [K[0]] [K[1]] :
+    K extends Tuple<3> ? T [K[0]] [K[1]] [K[2]] :
+    K extends Tuple<4> ? T [K[0]] [K[1]] [K[2]] [K[3]] :
+    K extends Tuple<5> ? T [K[0]] [K[1]] [K[2]] [K[3]] [K[4]] :
+    unknown;
+type Tuple<N> = readonly any[] & { length: N }
+
+const schema = {
+    definitions: {
+        id: { type: 'number' },
+        recursive: {
+            type: 'object',
+            properties: {
+                name: { type: 'string' },
+                values: {
+                    type: 'array',
+                    items: {
+                        '$ref': ['#', 'definitions', 'recursive'],
+                    },
+                },
+            },
+            required: ['name', 'values'],
+        },
+    },
     type: 'object',
     properties: {
         id: { type: 'string' },
@@ -44,11 +105,55 @@ const thing = {
             }, {
                 type: 'number',
             }]
+        },
+        enum: {
+            enum: ['a', 'b', null, true, 4],
+        },
+        rec: {
+            '$ref': ['#', 'definitions', 'recursive'],
+        },
+        all: {
+            allOf: [{
+                type: 'object',
+                properties: {
+                    name: { type: 'number' },
+                },
+                required: ['name'],
+            }, {
+                type: 'object',
+                properties: {
+                    id: { type: 'string' },
+                },
+                required: ['id'],
+            }]
         }
     },
-    required: ['id', 'name', 'other']
+    required: ['id', 'name', 'other'],
 } as const;
-const other = {
-    type: 'string',
-} as const;
-type Thing = ToInterface<typeof thing | typeof other>;
+
+type Schema = ToInterface<typeof schema>;
+const value: Schema = {
+    id: 'a',
+    name: 'c',
+    other: '',
+    all: {
+        name: 43,
+        id: '',
+    },
+    rec: {
+        name: '',
+        values: [{
+            name: '',
+            values: [{
+                name: '',
+                values: [{
+                    name: '',
+                    values: [{
+                        name: '',
+                        values: []
+                    }]
+                }]
+            }]
+        }]
+    }
+}
